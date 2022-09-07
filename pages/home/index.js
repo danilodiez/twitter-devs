7ee44a1f2cdb382bc55abba7406cbbb4b4a393f0
Danilo Diez <daniloo10.dd@gmail.com>
2022-09-07 16:56:52 -0500

    feat: first iteration of the tweets

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,14 +1,30 @@
 import styles from "./home.module.css";
-import Home from "../../pages/";
+import { useState, useEffect } from "react";
+import Avatar from "components/Avatar";
 
 export default function HomePage() {
+  const [timeline, setTimeline] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/statuses/home_timeline")
+      .then((res) => res.json())
+      .then(setTimeline);
+  });
+  console.log(timeline);
   return (
     <div>
       <header className={styles.header}>
         <h2 className={styles.h2}>Inicio</h2>
       </header>
       <section className={styles.section}>
-        <Home />
+        {/* <Home /> */}
+        {timeline.map((devtweet) => {
+          return (
+            <article key={devtweet.id}>
+              <Avatar src={devtweet.avatar} text={devtweet.name} />
+            </article>
+          );
+        })}
       </section>
       <nav className={styles.nav}>a</nav>
     </div>
